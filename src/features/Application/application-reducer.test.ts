d6c26296bf652d03affe5b38961797566f1bb80b
ToniNov <antoni.novik@yandex.ru>
2022-08-09 15:24:08 +0300

    fix test

--- src/features/Application/application-reducer.test.ts
+++ src/features/Application/application-reducer.test.ts
@@ -1,4 +1,8 @@
-import {applicationReducer, InitialStateType, setAppErrorAC, setAppStatusAC} from './application-reducer'
+import {appActions} from "../CommonActions/App";
+import {InitialStateType, slice} from "./application-reducer";
+
+const {reducer: appReducer} = slice
+const {setAppError, setAppStatus} = appActions
 
 let startState: InitialStateType;
 
@@ -12,14 +16,14 @@ beforeEach(() => {
 
 test('correct error message should be set', () => {
 
-    const endState = applicationReducer(startState, setAppErrorAC({error:'some error'}))
+    const endState = appReducer(startState, setAppError({error:'some error'}))
 
     expect(endState.error).toBe('some error');
 })
 
 test('correct status should be set', () => {
 
-    const endState = applicationReducer(startState, setAppStatusAC({status:'loading'}))
+    const endState = appReducer(startState, setAppStatus({status:'loading'}))
 
     expect(endState.status).toBe('loading');
 })
