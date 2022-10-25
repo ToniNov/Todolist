af78e9e748119bdca562e431cb0ef002d02663f7
ToniNov <antoni.novik@yandex.ru>
2022-10-25 16:48:02 +0300

    Updates

---
+++ precache-manifest.e65e86b92085ad3d50c1cb7d0e9e6991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7d0d233b825cebfa0548bd5ae15880",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "1ef2231cf8e2aba06482",
+    "url": "/todolist/static/css/main.4cb77ec2.chunk.css"
+  },
+  {
+    "revision": "1ad347e32c23eb50a666",
+    "url": "/todolist/static/js/2.099028cb.chunk.js"
+  },
+  {
+    "revision": "c9fe1276f8e30b0e9864b5fc7054b93f",
+    "url": "/todolist/static/js/2.099028cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef2231cf8e2aba06482",
+    "url": "/todolist/static/js/main.d1311d7b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/todolist/static/js/main.d1311d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ebc9bad2a2c9c79d0c",
+    "url": "/todolist/static/js/runtime-main.39f6e27a.js"
+  }
+]);
